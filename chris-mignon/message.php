08c41e77e4d1c3155126913f0d445e8e58ca2544
christopher mignon <dspmosfet@gmail.com>
2024-11-14 08:53:42 -0400

    Update message.php

--- chris-mignon/message.php
+++ chris-mignon/message.php
@@ -1,8 +1,8 @@
 <?php
 
-$con = mysqli_connect('mysql-1c358fde-dspmosfet-d487.e.aivencloud.com','avnadmin','AVNS_zZLlk6LjKRgeFt8NPYn');
+$con = mysqli_connect('localhost','u641204627_christopher','DSP@2007dsp');
 
-mysqli_select_db($con,'chris_db');
+mysqli_select_db($con,'u641204627_chris_db');
 
 $sql = "INSERT INTO Messages_DB(name,email,subject,message ) VALUES ('$_POST[name]', '$_POST[email]', '$_POST[subject]', '$_POST[message]')";
 
